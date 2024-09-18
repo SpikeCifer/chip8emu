ee384ae522dcd257560bd9d877fb968a0db1ea2a
SpikeCifer <stelioszapp+dev@gmail.com>
2024-09-18 19:58:28 +0300

    small modifications taking advantage of Zig labels

--- src/display.zig
+++ src/display.zig
@@ -12,31 +12,26 @@ const HEIGHT = 500;
 const BORDER = 20;
 
 pub fn create_window() void {
-    const Display = cx11.XOpenDisplay(null).?;
+    const Display = cx11.XOpenDisplay(null).?; // If the returned pointer is null, exit
     defer _ = cx11.XCloseDisplay(Display);
 
     const Screen = cx11.DefaultScreen(Display);
     const Window = cx11.XCreateSimpleWindow(Display, cx11.RootWindow(Display, Screen), POSX, POSY, WIDTH, HEIGHT, BORDER, cx11.BlackPixel(Display, Screen), cx11.WhitePixel(Display, Screen));
     _ = cx11.XMapWindow(Display, Window);
 
-    while (true) {
+    display_loop: while (true) {
         var Event: cx11.XEvent = undefined;
         _ = cx11.XNextEvent(Display, &Event);
+
         switch (Event.type) {
             cx11.Expose => {
                 _ = cx11.XFillRectangle(Display, Window, cx11.DefaultGC(Display, Screen), 20, 20, 10, 10);
             },
-            else => break,
+            else => continue :display_loop,
         }
     }
 }
 
 pub fn main() void {
     create_window();
-}
-
-test "Blitz Bits" {
-    // Create the display
-    create_window();
-    // Display the pixels buffer
 }
